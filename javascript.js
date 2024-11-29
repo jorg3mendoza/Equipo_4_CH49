cbf4533e3befb195a76cfe9a69c28cf928babe73
Liz Delgado <lizdelgadonav26@gmail.com>
2024-11-29 16:42:41 -0600

    Agregar archivo js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// console.log("Hello from JS external");
+// alert("Hola gente");
+// prompt("Ingresa algo");
+
+let nombre=prompt("Ingresa tu nombre: ");
+let apellido=prompt("Ingresa tu primer apellido: ");
+alert("Buenos dias " +nombre+ " " +apellido+ " que todo salga bien hoy :)♡ ");
